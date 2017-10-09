be1ea8c096c8bf5639bb368636766a93d9d81bc4
bapidas <bapi.das@vantagecircle.com>
2017-10-09 17:09:40 +0530

    First push

---
+++ app/src/main/java/com/vantagecircle/heartrate/AppComponent.java
@@ -0,0 +1,8 @@
+package com.vantagecircle.heartrate;
+
+/**
+ * Created by bapidas on 09/10/17.
+ */
+
+public interface AppComponent {
+}
---
+++ app/src/main/java/com/vantagecircle/heartrate/HeartApplication.java
@@ -0,0 +1,11 @@
+package com.vantagecircle.heartrate;
+
+import android.app.Application;
+
+/**
+ * Created by bapidas on 09/10/17.
+ */
+
+public class HeartApplication extends Application {
+
+}
